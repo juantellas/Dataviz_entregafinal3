46bc0b2fa8bc5c8b7e1b928e6ea46bd00e8c8290
Juan Aguirre <137755327+juantellas@users.noreply.github.com>
2025-11-13 00:19:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -620,4 +620,6 @@ def actualizar_metricas(modelo_seleccionado, modelos_comparar):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+       app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
+
+
